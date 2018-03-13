9d7966a676deb5a3ce75d6c0bffaef583acfe46f
xiaoxiangzi <xiaoxiangzi@users.noreply.github.com>
2018-03-13 13:01:56 +0800

    Update 1067.cpp
    uncomment main

--- 1067.cpp
+++ 1067.cpp
@@ -12,40 +12,40 @@
 #include <vector>
 using namespace std;
 
-//int main(int argc, const char * argv[]) {
-////        freopen("1067.txt", "r", stdin);
-////        freopen("1067_1.txt", "r", stdin);
-//    int n;
-//    string password;
-//    vector<string> inputs;
-//    cin >> password >> n;
-//    int bufSize = 1024;
-//    char buf[bufSize];
-//    cin.getline(buf, bufSize);
-//    cin.getline(buf, bufSize);
-//    string input(buf);
-//    int tryCount = 0;
-//    bool succeed = false;
-//    while (input != "#") {
-//        if (input == password) {
-//            succeed = true;
-//            cout << "Welcome in" << endl;
-//            break;
-//        } else {
-//            cout << "Wrong password: " << input << endl;
-//        }
-//        tryCount++;
-//        
-//        if (tryCount >= n && !succeed) {
-//            cout << "Account locked" << endl;
-//            break;
-//        }
-//        
-//        cin.getline(buf, bufSize);
-//        input.clear();
-//        input.append(buf);
-//    }
-//    
-//    return 0;
-//}
+int main(int argc, const char * argv[]) {
+//        freopen("1067.txt", "r", stdin);
+//        freopen("1067_1.txt", "r", stdin);
+   int n;
+   string password;
+   vector<string> inputs;
+   cin >> password >> n;
+   int bufSize = 1024;
+   char buf[bufSize];
+   cin.getline(buf, bufSize);
+   cin.getline(buf, bufSize);
+   string input(buf);
+   int tryCount = 0;
+   bool succeed = false;
+   while (input != "#") {
+       if (input == password) {
+           succeed = true;
+           cout << "Welcome in" << endl;
+           break;
+       } else {
+           cout << "Wrong password: " << input << endl;
+       }
+       tryCount++;
+       
+       if (tryCount >= n && !succeed) {
+           cout << "Account locked" << endl;
+           break;
+       }
+       
+       cin.getline(buf, bufSize);
+       input.clear();
+       input.append(buf);
+   }
+   
+   return 0;
+}
 
