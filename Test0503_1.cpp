9ed356b241b71c3802443c0911ed95d296c61f3c
xiaoxiangzi <leijing.yang@gmail.com>
2018-05-03 16:01:06 +0800

    性能优化

--- Test0503_1.cpp
+++ Test0503_1.cpp
@@ -14,6 +14,7 @@ using namespace std;
 
 struct Vertex {
     int id;
+    bool hasEdges;
     unordered_map<int, bool> edges;
 };
 
@@ -24,6 +25,7 @@ public:
         for (int i = 1; i <= n; i++) {
             Vertex v;
             v.id = i;
+            v.hasEdges = false;
             _vertexes.insert(pair<int, Vertex>(i, v));
         }
         
@@ -35,11 +37,12 @@ public:
             auto& v2 = _vertexes.at(v2Id);
             v1.edges[v2Id] = true;
             v2.edges[v1Id] = true;
+            v1.hasEdges = true;
+            v2.hasEdges = true;
         }
     }
     
-    bool canConquer(const vector<int>& cities) {
-        unordered_map<int, Vertex> vertexes = _vertexes;
+    bool canConquer(const unordered_map<int, bool>& cities) {
         if (cities.empty()) {
             return true;
         }
@@ -48,38 +51,39 @@ public:
             return true;
         }
         
-        for (int i = 0; i < cities.size(); i++) {
-            auto iter = vertexes.find(cities[i]);
-            if (iter != vertexes.end()) {
-                vertexes.erase(iter);
-            }
-        }
-        
-        for (auto iter = vertexes.begin(); iter != vertexes.end(); iter++) {
-            auto& v = iter->second;
-            removeEdgesOfCities(v, cities);
-        }
-        
         bool can = true;
-        for (auto iter = vertexes.begin(); iter != vertexes.end(); iter++) {
+        for (auto iter = _vertexes.begin(); iter != _vertexes.end(); iter++) {
             auto& v = iter->second;
-            if (!v.edges.empty()) {
-                can = false;
-                break;
-            }
-        }
-        return can;
-    }
-private:
-    void removeEdgesOfCities(Vertex& v, const vector<int>& cities) {
-        for (int i = 0; i < cities.size(); i++) {
-            auto iter = v.edges.find(cities[i]);
-            if (iter != v.edges.end()) {
-                v.edges.erase(iter);
-                if (v.edges.empty()) {
+            if (cities.find(v.id) == cities.end()) {
+                v.hasEdges = v.edges.size() > 0;
+                removeEdgesOfCities(v, cities);
+                if (v.hasEdges) {
+                    can = false;
                     break;
                 }
             }
+            
+        }
+        
+        return can;
+    }
+private:
+    void removeEdgesOfCities(Vertex& v, const unordered_map<int, bool>& cities) {
+        int delCount = 0;
+        int edgesSize = (int)v.edges.size();
+        for (auto cityIter = cities.begin(); cityIter != cities.end(); cityIter++) {
+            int city = cityIter->first;
+            auto iter = v.edges.find(city);
+            if (iter != v.edges.end()) {
+                delCount++;
+                if (delCount == edgesSize) {
+                    break;
+                }
+            }
+        }
+        
+        if (delCount == edgesSize) {
+            v.hasEdges = false;
         }
     }
     unordered_map<int, Vertex> _vertexes;
@@ -97,13 +101,12 @@ int main(int argc, const char * argv[]) {
     for (int i = 0; i < k; i++) {
         int num = 0;
         cin >> num;
-        vector<int> vertexes;
-        vertexes.resize(num);
+        unordered_map<int, bool> vertexes;
         
         for (int j = 0; j < num; j++) {
             int vId = 0;
             cin >> vId;
-            vertexes[j] = vId;
+            vertexes[vId] = true;
         }
         
         if (g.canConquer(vertexes)) {
