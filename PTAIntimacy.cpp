6b0095c013e81b6bfd752bcb0728e9d83d029d26
xiaoxiangzi <leijing.yang@gmail.com>
2018-05-18 20:40:13 +0800

    亲密度

--- PTAIntimacy.cpp
+++ PTAIntimacy.cpp
@@ -15,33 +15,60 @@
 #include <sstream>
 using namespace std;
 
+typedef enum {
+    MALE,
+    FEMALE,
+} Sex;
+
 struct Intimacy {
-    string id;
+    int id;
     float intimacy;
 };
 
 struct Person {
-    string id;
-    unordered_map<string, Intimacy> intimacyMap;
+    int id;
+    Sex sex;
+    unordered_map<int, Intimacy> intimacyMap;
 };
 
-void addIncimacyInList(vector<Person*>& persons) {
+void readPersonInfo(int& id, Sex& sex) {
+    string s;
+    cin >> s;
+    if (s[0] == '-') {
+        sex = FEMALE;
+    } else {
+        sex = MALE;
+    }
+    id = abs(atoi(s.c_str()));
+}
+
+void addIncimacyInList(vector<Person*>& persons, const Person& pa, const Person& pb) {
     int k = (int) persons.size();
     float intimacy = 1.0 / k;
     for (int i = 0; i < k; i++) {
         Person* a = persons[i];
+        if (a->id != pa.id && a->id != pb.id) {
+            continue;
+        }
+        
         for (int j = 0; j < k; j++) {
             if (i == j) {
                 continue;
             }
+            
             Person* b = persons[j];
-            auto iter = b->intimacyMap.find(a->id);
-            if (iter != b->intimacyMap.end()) {
+            if (a->sex == b->sex) {
+                continue;
+            }
+            
+            auto iter = a->intimacyMap.find(b->id);
+            if (iter != a->intimacyMap.end()) {
                 iter->second.intimacy += intimacy;
             } else {
                 Intimacy item;
-                item.id = a->id;
-                b->intimacyMap.insert(make_pair(a->id, item));
+                item.id = b->id;
+                item.intimacy = intimacy;
+                a->intimacyMap.insert(make_pair(b->id, item));
             }
         }
     }
@@ -49,18 +76,29 @@ void addIncimacyInList(vector<Person*>& persons) {
 
 bool compareIntimacy(Intimacy& a, Intimacy& b) {
     if (abs(a.intimacy - b.intimacy) < 0.0000001) {
-        return abs(atoi(a.id.c_str())) < abs(atoi(b.id.c_str()));
+        return a.id < b.id;
     }
     return a.intimacy > b.intimacy;
 }
 
-void printIntimacyList(const Person& person, const vector<Intimacy>& intimacyList) {
+void printPerson(const Person& p) {
+    if (p.sex == FEMALE) {
+        cout << "-";
+    }
+    cout << p.id;
+}
+
+void printIntimacyList(const Person& person, const vector<Intimacy>& intimacyList, const unordered_map<int, Person>& personsMap) {
     for (int i = 0; i < intimacyList.size(); i++) {
-        cout << person.id << " " << intimacyList[i].id << endl;
+        auto& p = personsMap.at(intimacyList[i].id);
+        printPerson(person);
+        cout << " ";
+        printPerson(p);
+        cout << endl;
     }
 }
 
-vector<Intimacy> findIntimacyList(string id, const unordered_map<string, Person>& personsMap, string checkId, bool& hasCheckId) {
+vector<Intimacy> findIntimacyList(int id, const unordered_map<int, Person>& personsMap, int checkId, bool& hasCheckId) {
     vector<Intimacy> result;
     auto& person = personsMap.at(id);
     for (auto iter = person.intimacyMap.begin(); iter != person.intimacyMap.end(); iter++) {
@@ -90,18 +128,21 @@ vector<Intimacy> findIntimacyList(string id, const unordered_map<string, Person>
 }
 
 int main(int argc, const char * argv[]) {
-    freopen("PTAIntimacy.txt", "r", stdin);
+//    freopen("PTAIntimacy.txt", "r", stdin);
     int n = 0;
     int m = 0;
     cin >> n >> m;
-    unordered_map<string, Person> persons;
+    unordered_map<int, Person> persons;
+    vector<vector<Person*>> pics;
+    pics.resize(m);
     for (int i = 0; i < m; i++) {
         int k = 0;
         cin >> k;
-        vector<Person*> personList;
+        vector<Person*>& personList = pics[i];
         for (int j = 0; j < k; j++) {
-            string id;
-            cin >> id;
+            int id;
+            Sex sex;
+            readPersonInfo(id, sex);
             auto iter = persons.find(id);
             Person* p = nullptr;
             if (iter != persons.end()) {
@@ -109,28 +150,56 @@ int main(int argc, const char * argv[]) {
             } else {
                 Person person;
                 person.id = id;
+                person.sex = sex;
                 persons.insert(make_pair(id, person));
                 p = &persons.at(id);
             }
             personList.push_back(p);
         }
-        addIncimacyInList(personList);
     }
     
-    string aId;
-    string bId;
-    cin >> aId >> bId;
+    int aId;
+    Sex aSex;
+    readPersonInfo(aId, aSex);
+    int bId;
+    Sex bSex;
+    readPersonInfo(bId, bSex);
+    auto iter = persons.find(aId);
+    if (iter == persons.end()) {
+        Person person;
+        person.id = aId;
+        person.sex = aSex;
+        persons.insert(make_pair(aId, person));
+    }
+    
+    iter = persons.find(bId);
+    if (iter == persons.end()) {
+        Person person;
+        person.id = bId;
+        person.sex = bSex;
+        persons.insert(make_pair(bId, person));
+    }
+    
+    Person& pa = persons[aId];
+    Person& pb = persons[bId];
+    for (int i = 0; i < m; i++) {
+        vector<Person*>& personList = pics[i];
+        addIncimacyInList(personList, pa, pb);
+    }
     
     bool hasB = false;
     bool hasA = false;
     vector<Intimacy> aList = findIntimacyList(aId, persons, bId, hasB);
     vector<Intimacy> bList = findIntimacyList(bId, persons, aId, hasA);
     
-    if (hasB && hasA) {
-        cout << aId << " " << bId << endl;
+    if ((hasB && hasA) || (aList.empty() && bList.empty())) {
+        printPerson(pa);
+        cout << " ";
+        printPerson(pb);
+        cout << endl;
     } else {
-        printIntimacyList(persons.at(aId), aList);
-        printIntimacyList(persons.at(bId), bList);
+        printIntimacyList(persons.at(aId), aList, persons);
+        printIntimacyList(persons.at(bId), bList, persons);
     }
     
     return 0;
